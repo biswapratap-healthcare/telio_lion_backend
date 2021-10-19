03be817fa34b49393c50bd73f575acd1388cfa74
biswapratap-healthcare <64803030+biswapratap-healthcare@users.noreply.github.com>
2021-10-19 22:32:50 +0530

    prepare train data 2

--- prepare_train_data.py
+++ prepare_train_data.py
@@ -13,12 +13,16 @@ if __name__ == "__main__":
     whisker_dir = os.path.join(data_dir, 'whisker')
     if os.path.exists(data_dir):
         shutil.rmtree(data_dir)
+    if os.path.exists('face.csv'):
+        os.remove('face.csv')
+    if os.path.exists('whisker.csv'):
+        os.remove('whisker.csv')
     os.mkdir(data_dir)
     os.mkdir(face_dir)
     os.mkdir(whisker_dir)
     face_df = pd.DataFrame(columns=['Image', 'Id'])
     whisker_df = pd.DataFrame(columns=['Image', 'Id'])
-    source_dir = ''
+    source_dir = 'Preprocessed_Images'
     lion_name_dirs = os.listdir(source_dir)
     for lion_name_dir in lion_name_dirs:
         root_dir = os.path.join(source_dir, lion_name_dir)
@@ -27,9 +31,9 @@ if __name__ == "__main__":
             lion_image_path = os.path.join(root_dir, lion_image)
             prefix = ''.join(lion_image.split('.')[:-1])
             ext = lion_image.split('.')[-1]
-            lion_face_filename = lion_name_dir + '_' + prefix + '_face' + ext
+            lion_face_filename = lion_name_dir + '_' + prefix + '_face.' + ext
             lion_face_filepath = os.path.join(face_dir, lion_face_filename)
-            lion_whisker_filename = lion_name_dir + '_' + prefix + '_whisker' + ext
+            lion_whisker_filename = lion_name_dir + '_' + prefix + '_whisker.' + ext
             lion_whisker_filepath = os.path.join(whisker_dir, lion_whisker_filename)
             tmp_dir = None
             try:
@@ -54,3 +58,5 @@ if __name__ == "__main__":
                 if tmp_dir and os.path.exists(tmp_dir):
                     shutil.rmtree(tmp_dir)
                 continue
+    face_df.to_csv('face.csv', index=False)
+    whisker_df.to_csv('whisker.csv', index=False)
